1a0118490b476241f73d3704b5be77c7b96adb6a
Esmatullah <esmatqadeemi12@gmail.com>
2023-07-31 19:45:56 +0100

    using useState in the react application

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
-import React from "react";
-import './ExpenseDate'
+import React, {useState} from "react";
 import './ExpenseItem.css'
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import { set } from "lodash";
 
-function ExpenseItem(props){
-        return(
-        <Card className="expense-item">
-            <ExpenseDate date = {props.date} />
-            <div className="expense-item-desc">
-                <h2 className="expense-title">{props.title}</h2>
-                <div className="expense-amount">${props.amount}</div>
-            </div>
-        </Card>
+
+
+const ExpenseItem = (props) =>{
+    const [newTitle, setTitle] = useState(props.title)
+
+    const clickHandler = () =>{
+        setTitle("Updated!!")
+        console.log("hello!!")
+    }
+    return(
+    <Card className="expense-item">
+        <ExpenseDate date = {props.date} />
+        <div className="expense-item-desc">
+            <h2 className="expense-title">{newTitle}</h2>
+            <div className="expense-amount">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Click</button>
+    </Card>
     )
 }
 export default ExpenseItem  
