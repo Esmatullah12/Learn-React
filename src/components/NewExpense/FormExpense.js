9093341f1a2aefb7dcc621a950ac433fb98eac2b
Esmatullah <esmatqadeemi12@gmail.com>
2023-08-02 07:31:26 +0100

    modify the style of the form

--- src/components/NewExpense/FormExpense.js
+++ src/components/NewExpense/FormExpense.js
@@ -7,17 +7,14 @@ const FormExpense = () =>{
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <br></br>
                     <input type="text"></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <br></br>
                     <input type="number" min='0.01' step='0.01'></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <br></br>
                     <input type="date" min='2020-1-1' max='2028-1-1'></input>
                 </div>
                 <div className="new-expense__action">
